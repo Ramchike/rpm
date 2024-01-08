5a7ca2299bcfd997e6a90bcffb7618eb04229283
Ramir Vorobyev <rrramirrr@yandex.ru>
2024-01-08 22:44:38 +0300

    update visual

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import json
 import random
 import time
+import inquirer
+
 from abc import abstractmethod
 from abc import ABC
 from enum import Enum
 from dataclasses import dataclass
 from typing import Optional, List
-import inquirer
+from math import trunc
+
+
+def animatePrint(text):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(0.08)
+    print()
 
 class Result(Enum):
     WIN = 1
@@ -62,6 +71,10 @@ class User(Player):
         )
         answer = inquirer.prompt([choices])
         return answer['choice']
+    
+    def editUserName(self) -> str:
+        self.username = input("Введи желаемое имя пользователя: ")
+        return self.username
 
 class Game:
 
@@ -71,23 +84,34 @@ class Game:
         self.ai = ai
 
     def start(self, round_count):
+        result_game = 0
         for i in range(round_count):
             player_pick = self.player.getPick()
             ai_pick = self.ai.getPick()
-            
             if player_pick == ai_pick:
                 round_result = Result.DRAW
             elif (player_pick.value == 0 and ai_pick.value == 1) or (player_pick.value == 1 and ai_pick.value == 2) or (player_pick.value == 2 and ai_pick.value == 0):
                 round_result = Result.WIN
+                result_game += 1
             else:
                 round_result = Result.LOSE
+                result_game += -1
             print(round_result.name)
             round = Round(round_result, player_pick, ai_pick)
             round_data = round.to_json()
-            print(json.dumps(round_data, indent=1))
             self.rounds.append(round_data)
-
-
+        if result_game >= 1:
+            result_game = Result.WIN
+        elif result_game == 0:
+            result_game = Result.DRAW
+        else:
+            result_game = Result.LOSE
+        return { 
+            "result": result_game.name,
+            "money_win": 0,
+            "rounds": self.rounds
+            }
+    
 class App:
 
     def __init__(self):
@@ -112,14 +136,30 @@ class App:
         ]
     )
         answer = inquirer.prompt([count_rounds])
-        game.start(answer['count'])
+        data_game = game.start(answer['count'])
+        print(data_game)
+        print(json.dumps(data_game, indent=1))
 
     def showStat(self):
-        print("stat")
-        print(json.dumps(self.data, indent=4))
-
+        data = self.data
+        animatePrint(f"Имя пользователя: {data['username']}")
+        animatePrint(f"Количество игр: {data['count_game']}")
+        animatePrint(f"Побед: {data['wins']}")
+        animatePrint(f"Поражений: {data['loses']}")
+        animatePrint(f"Процент побед: {trunc((data['wins'] / data['count_game']) * 100)}%")
+        animatePrint(f"Монет: {data['money']}")
+        animatePrint("История игр:")
     def showSettings(self):
         print("settings")
+        settings = inquirer.List('setting', 
+        message = "Настройки:",
+        choices = [
+            ("Поменять имя пользователя", self.player.editUserName),
+            ("Выбрать тему", 1)
+        ]
+    )
+        answer = inquirer.prompt([settings])
+        answer['setting']()
     
     def exitApp(self):
         print("exit")
